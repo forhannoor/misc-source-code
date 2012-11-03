98529da6c2d0448d69dd9a8ad27658747a2cc2c6
Forhan Noor <mac_bd@yahoo.com>
2012-11-03 12:44:30 -0700

    Connect4: drop_chip, win_check fixed, game_over

--- Connect4_ai/source.cpp
+++ Connect4_ai/source.cpp
@@ -14,11 +14,14 @@
 using namespace std;
 
 
-void initialize();                                 /* initializes the board */
-int board[board_height][board_width];	           /* connect4 board */
+int filled_counter=42;                  /* keeps the number of fields left blank */
+void initialize();                      /* initializes the board */
+int board[board_height][board_width];   /* connect4 board */
 int players[2]={0,1};
-int win_check(int row, int col, int player);	   /* checks for winning move and determines who won */
-void print_board();                                /* prints the board */
+int win_check(int player);	            /* checks for winning move and determines who won */
+void print_board();                     /* prints the board */
+bool game_over();                       /* checks if game is over */
+int drop_chip(int player, int col);     /* drops a chip in a column if available */
 
 
 int main()
@@ -29,26 +32,47 @@ int main()
 }
 
 
-int win_check(int row, int col, int player)
+int drop_chip(int player, int col)
 {
-	/* checks horizontally */
-	if(col+3<board_width)
-		if(board[row][col]==board[row][col+1]==board[row][col+2]==board[row][col+3]==player)
-			return player;
+    int status=0;
 
-	/* checks vertically */
-	if(row+3<board_height)
-        if(board[row][col]==board[row+1][col]==board[row+2][col]==board[row+3][col]==player)
-             return player;
+    for(int i=board_height-1; i>=0; i--)
+        if(board[i][col-1]==blank && status==0)
+        {
+            board[i][col-1]=player;
+            filled_counter-=1;
+            status=1;
+        }
 
-    /* checks ascending diagonals */
-    if(row+3<board_height && col+3<board_width)
-        if(board[row][col]==board[row+1][col+1]==board[row+2][col+2]==board[row+3][col+3]==player)
-             return player;
+    return status;
+}
 
-    /* checks descending diagonals */
-    if(row-3>=0 && col+3<board_width)
-        if(board[row][col]==board[row-1][col+1]==board[row-2][col+2]==board[row-3][col+3]==player)
+
+int win_check(int player)
+{
+    for(int row=0; row<board_height; row++)
+        for(int col=0; col<board_width; col++)
+        {
+            /* checks horizontally */
+            if(col+3<board_width)
+                if(board[row][col]==board[row][col+1]==board[row][col+2]==board[row][col+3]==player)
+                    return player;
+        
+            /* checks vertically */
+            if(row+3<board_height)
+                if(board[row][col]==board[row+1][col]==board[row+2][col]==board[row+3][col]==player)
+                     return player;
+        
+            /* checks ascending diagonals */
+            if(row+3<board_height && col+3<board_width)
+                if(board[row][col]==board[row+1][col+1]==board[row+2][col+2]==board[row+3][col+3]==player)
+                     return player;
+        
+            /* checks descending diagonals */
+            if(row-3>=0 && col+3<board_width)
+                if(board[row][col]==board[row-1][col+1]==board[row-2][col+2]==board[row-3][col+3]==player)
+                    return player;
+        }
 
     return -1;
 }
@@ -56,6 +80,7 @@ int win_check(int row, int col, int player)
 
 void print_board()
 {
+    cout<<endl;
     cout<<" - - - - - - - - - - - - - - "<<endl;
 
     for(int i=0; i<board_height; i++)
@@ -64,14 +89,21 @@ void print_board()
             cout<<"| ";
 
             if(board[i][j]==blank)
-            cout<<"- ";
+                cout<<"- ";
 
-            if(j==6)            
+            else if(board[i][j]==1)
+                cout<<"X ";
+
+            else if(board[i][j]==0)
+                cout<<board[i][j]<<" ";
+
+            if(j==6)
                 cout<<endl;         /* prints new line at the end of each row */
         }
 
     cout<<" - - - - - - - - - - - - - - "<<endl;
     cout<<"  1   2   3   4   5   6   7 "<<endl;
+    cout<<endl;
 }
 
 
@@ -80,4 +112,14 @@ void initialize()
     for(int i=0; i<board_height; i++)
         for(int j=0; j<board_width; j++)
             board[i][j]=blank;
+}
+
+
+bool game_over()
+{
+    if(filled_counter==0)
+        return true;
+
+    else
+        return false;
 }
