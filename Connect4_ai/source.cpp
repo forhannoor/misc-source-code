ae7e4dd8126d955e7135e742cb118c1c6e9c4864
Forhan Noor <mac_bd@yahoo.com>
2012-11-09 19:39:49 -0800

    typedef game_state, stack <states>, game_won(), generate_moves(), alpha_beta, undo(), evaluate() added

--- Connect4_ai/source.cpp
+++ Connect4_ai/source.cpp
@@ -6,19 +6,29 @@
 
 
 #include <iostream>
+#include <stack>
 
-#define board_width 7
-#define board_height 6
-#define blank -1
+#define BOARD_WIDTH 7
+#define BOARD_HEIGHT 6
+#define BLANK -1
 
 using namespace std;
 
 
-int filled_counter=42;                  /* keeps the number of fields left blank */
+typedef struct
+{
+    int x;          /* index i */
+    int y;          /* index j */
+    int score;      /* evaluation score for this state */
+} game_state;
+
+stack <game_state> states;
+int filled_counter=42;                  /* keeps the number of fields left BLANK */
 void initialize();                      /* initializes the board */
-int board[board_height][board_width];   /* connect4 board */
+int board[BOARD_HEIGHT][BOARD_WIDTH];   /* connect4 board */
 int players[2]={0,1};
 int win_check(int player);	            /* checks for winning move and determines who won */
+bool game_won();
 void print_board();                     /* prints the board */
 bool is_tie();                          /* checks if game is tie */
 int drop_chip(int player, int col);     /* drops a chip in a column if available */
@@ -27,18 +37,33 @@ void announce_winner(int p);            /* announces game result */
 void take_move(int p);
 void human_move();
 void computer_move();
+int evaluator();                        /* evaluates game state */
 void test();                            /* test purpose data initialization */
+void generate_moves();                  /* generates moves */
+int score[BOARD_HEIGHT][BOARD_WIDTH];
+void print_score();
+int getScore();
+int max_score=1000000;
+int invalid_score=999999;
+int alpha_beta(int player, int alpha=-invalid_score, int beta=invalid_score, int depth=0);
+int last_x;
+int last_y;
+void undo(int i=last_x, int j=last_y);  /* undo the last move */
+void print_states();
+int evaluate(int player, int level=0, int alpha=-invalid_score, int beta=invalid_score);
 
 
 int drop_chip(int player, int col)
 {
     int status=0;   /* 0 => failed, 1=> success */
 
-    for(int i=board_height-1; i>=0; i--)
-        if(board[i][col-1]==blank && status==0)
+    for(int i=BOARD_HEIGHT-1; i>=0; i--)
+        if(board[i][col-1]==BLANK && status==0)
         {
             board[i][col-1]=player;
             filled_counter-=1;
+            last_x=i;
+            last_y=col-1;
             status=1;
         }
 
@@ -48,27 +73,27 @@ int drop_chip(int player, int col)
 
 int win_check(int player)
 {
-    for(int row=0; row<board_height; row++)
-        for(int col=0; col<board_width; col++)
+    for(int row=0; row<BOARD_HEIGHT; row++)
+        for(int col=0; col<BOARD_WIDTH; col++)
         {
             /* checks horizontally */
-            if(col+3<board_width)
+            if(col+3<BOARD_WIDTH)
                 if(board[row][col]==player && board[row][col+1]==player && board[row][col+2]==player && board[row][col+3]==player)
                     return player;
-                    
-        
+
+
             /* checks vertically */
-            if(row+3<board_height)
+            if(row+3<BOARD_HEIGHT)
                 if(board[row][col]==player && board[row+1][col]==player && board[row+2][col]==player && board[row+3][col]==player)
                     return player;
-        
+
             /* checks ascending diagonals */
-            if(row+3<board_height && col+3<board_width)
+            if(row+3<BOARD_HEIGHT && col+3<BOARD_WIDTH)
                 if(board[row][col]==player && board[row+1][col+1]==player && board[row+2][col+2]==player && board[row+3][col+3]==player)
                     return player;
-        
+
             /* checks descending diagonals */
-            if(row-3>=0 && col+3<board_width)
+            if(row-3>=0 && col+3<BOARD_WIDTH)
                 if(board[row][col]==player && board[row-1][col+1]==player && board[row-2][col+2]==player && board[row-3][col+3]==player)
                     return player;
         }
@@ -77,17 +102,27 @@ int win_check(int player)
 }
 
 
+bool game_won()
+{
+    if(win_check(0)==0 || win_check(1)==1)
+        return true;
+
+    else
+        return false;
+}
+
+
 void print_board()
 {
     cout<<endl;
     cout<<" - - - - - - - - - - - - - - "<<endl;
 
-    for(int i=0; i<board_height; i++)
-        for(int j=0; j<board_width; j++)
+    for(int i=0; i<BOARD_HEIGHT; i++)
+        for(int j=0; j<BOARD_WIDTH; j++)
         {
             cout<<"| ";
 
-            if(board[i][j]==blank)
+            if(board[i][j]==BLANK)
                 cout<<"- ";
 
             else if(board[i][j]==1)
@@ -108,15 +143,26 @@ void print_board()
 
 void initialize()
 {
-    for(int i=0; i<board_height; i++)
-        for(int j=0; j<board_width; j++)
-            board[i][j]=blank;
+    for(int i=0; i<BOARD_HEIGHT; i++)
+        for(int j=0; j<BOARD_WIDTH; j++)
+            board[i][j]=BLANK;
+
+    for(int i=0; i<BOARD_HEIGHT; i++)
+        for(int j=0; j<BOARD_WIDTH; j++)
+            score[i][j]=0;
 }
 
 
 bool is_tie()
 {
-    if(filled_counter==0)
+    int counter=0;
+
+    for(int i=0; i<6; i++)
+        for(int j=0; j<7; j++)
+            if(board[i][j]==BLANK)
+                counter++;
+
+    if(counter==0 && game_won()==false)
         return true;
 
     else
@@ -143,12 +189,16 @@ void game_playing()
 
         take_move(turn_for);
         i++;
-        w=win_check(turn_for);
 
-        if(w!=-1)
-            break;
+        if(game_won())
+            break;            
+        // w=win_check(turn_for);
+
+        // if(w!=-1)
+        //     break;
     }
-    
+
+    w=(win_check(0)==0) ? 0 : 1; 
     announce_winner(w);
 }
 
@@ -156,7 +206,7 @@ void game_playing()
 void announce_winner(int p)
 {
     print_board();
-    
+
     switch(p)
     {
         case 0:
@@ -205,22 +255,283 @@ void human_move()
 
 void computer_move()
 {
-    drop_chip(1,7);
+    int x=drop_chip(1,7);
+
+    if(x==0)
+        x=drop_chip(1,6);
+}
+
+
+int evaluator()
+{
+    if(win_check(0)==0)
+        return 1000000;
+
+    else if(win_check(1)==1)
+        return -1000000;
+
+    else
+        return 0;
+}
+
+
+void print_score()
+{
+    for(int i=0; i<BOARD_HEIGHT; i++)
+        for(int j=0; j<BOARD_WIDTH; j++)
+        {
+            cout<<score[i][j]<<"\t";
+
+            if(j==BOARD_WIDTH-1)
+                cout<<endl;
+        }
 }
 
 
 int main()
 {
     initialize();
-    game_playing();
+    print_board();
+    // game_playing();
+    // initialize();
+    // print_board();
+    // test();
+    // print_board();
+    // generate_moves();
+    // print_states();
+    // game_playing();
     return 0;
 }
 
 
 void test()
 {
-    board[1][3]=1;
-    board[2][4]=1;
-    board[3][5]=1;
+    board[5][0]=0;
+    board[5][1]=1;
+    board[5][2]=0;
+    board[5][3]=1;
+    board[5][4]=0;
+    board[5][5]=1;
+    board[5][6]=0;
+
+    board[4][0]=1;
+    board[4][1]=0;
+    board[4][2]=1;
+    board[4][3]=0;
+    board[4][4]=1;
+    board[4][5]=0;
     board[4][6]=1;
+
+    board[3][0]=0;
+    board[3][1]=1;
+    board[3][2]=0;
+    board[3][3]=1;
+    board[3][4]=0;
+    board[3][5]=1;
+    board[3][6]=0;
+
+    board[2][0]=0;
+    board[2][1]=1;
+    board[2][2]=0;
+    board[2][3]=1;
+    board[2][4]=0;
+    board[2][5]=1;
+    board[2][6]=0;
+}
+
+
+int getScore()
+{
+    int s = 0;
+
+    //Pieces in the middle score higher than
+    //pieces at the edges
+
+    //Here's how the positions are scored:
+    //
+    //¦1¦2¦3¦4¦3¦2¦1¦
+    //¦2¦3¦4¦5¦4¦3¦2¦
+    //¦3¦4¦5¦6¦5¦4¦3¦
+    //¦2¦3¦4¦5¦4¦3¦2¦
+    //¦1¦2¦3¦4¦3¦2¦1¦
+    //¦0¦1¦2¦3¦2¦1¦0¦
+    //+-------------+
+
+    for (int x=0; x<BOARD_WIDTH; x++)
+    {
+        int columnscore = (BOARD_WIDTH/2) - x;
+        if (columnscore < 0) columnscore = -columnscore;
+        columnscore = (BOARD_WIDTH/2) - columnscore;
+
+        //Count the number of pieces in each column
+        //and score accordingly
+        for (int y=0; y<BOARD_HEIGHT; y++)
+        {
+            int rowscore = (BOARD_HEIGHT/2) - y;
+            if (rowscore < 0) rowscore = -rowscore;
+            rowscore = (BOARD_HEIGHT/2) - rowscore;
+
+            if (board[x][y]==1)
+                s += columnscore+rowscore;
+
+            else if (board[x][y]==0)
+                s -= columnscore+rowscore;
+
+            score[y][x]=s;
+        }
+    }
+
+    return 0;
+}
+
+
+// int alpha_beta(int player, int alpha, int beta, int depth)
+// {
+//     if(win_check(0)==0)
+//         return max_score;
+
+//     else if(win_check(1)==1)
+//         return -max_score;
+
+//     if(depth>=4)
+//     {
+//         return evaluator();
+//     }
+
+//     if(player==0)
+//     {
+//         int max_score=-invalid_score;
+//         int s;
+
+//         for(int i=0; i<BOARD_HEIGHT; i++)
+//             for(int j=0; j<BOARD_WIDTH; j++)
+//             {
+//                 if(board[i][j]==BLANK)
+//                 {
+//                     board[i][j]=0;
+//                     s=evaluator();
+//                     board[i][j]=BLANK;
+
+//                     if(s>alpha)
+//                         alpha=s;
+//                 }
+//             }
+
+//         return alpha;
+//     }
+
+//     else if(player==1)
+//     {
+//         int min_score=invalid_score;
+//         int s;
+
+//         for(int i=0; i<BOARD_HEIGHT; i++)
+//             for(int j=0; j<BOARD_WIDTH; j++)
+//             {
+//                 if(board[i][j]==BLANK)
+//                 {
+//                     board[i][j]=0;
+//                     s=evaluator();
+//                     board[i][j]=BLANK;
+
+//                     if(s<beta)
+//                         beta=s;
+//                 }
+//             }
+
+//         return alpha;
+//     }
+// }
+
+
+void generate_moves()
+{
+    for(int col=1; col<=BOARD_WIDTH; col++)
+    {
+        int status=drop_chip(0,col);        /* assume a move for opponent */
+        int t1,t2;
+
+        if(status==1)
+        {
+            t1=last_x;                      /* keep track of last assumed move by opponent */ 
+            t2=last_y;
+    
+            for(int i=1; i<=BOARD_WIDTH; i++)
+            {
+                if(drop_chip(1,i)==1)       /* generate move based on that assumed move */
+                {
+                    game_state g;
+                    int eval=evaluator();   /* evaluate that move */
+                    g.x=last_x;             /* keep track of that move*/
+                    g.y=last_y;             
+                    g.score=eval;           /* keep that evalutation */
+                    states.push(g);         /* store that game state */
+                    print_board();
+                    undo();                 /* undo this move */
+                }
+            }
+
+            undo(t1,t2);                    /* undo opponents assumed move */
+        }
+    }
+}
+
+
+void undo(int i, int j)
+{
+    board[i][j]=BLANK;
+    filled_counter+=1;
+}
+
+
+void print_states()
+{
+    while(!states.empty())
+    {
+        game_state temp=states.top();
+        states.pop();
+        cout<<"X:"<<temp.x<<endl;
+        cout<<"Y:"<<temp.y<<endl;
+        cout<<"Score:"<<temp.score<<endl;
+    }
+}
+
+
+int evaluate(int player, int level, int alpha, int beta)
+{
+    int i, goodness, best, maxab;
+
+    if (level == 4)
+        return evaluator();
+    
+    else 
+    {
+        best = -invalid_score;
+        maxab = alpha;
+
+        for(i=0; i<BOARD_WIDTH; i++) 
+        {
+            if(drop_chip(player,i)==0) 
+                continue;
+
+            else if(win_check(player)==player)
+                goodness=max_score-4;
+
+            else
+                goodness = evaluate((player+1)%2, level, -beta, -maxab);
+            
+            if (goodness > best) 
+            {
+                best = goodness;
+                
+                if (best > maxab)
+                    maxab = best;
+            }
+
+            if (best > beta)
+                break;
+        }
+
+        return -best;
+    }
 }
