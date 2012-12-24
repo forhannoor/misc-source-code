8dbf9b43a4f96f247095aa6484fab80d24b96c34
Forhan Noor <mac_bd@yahoo.com>
2012-12-24 10:41:26 -0800

    css modified, login page modified

--- web-projects/blog/application/views/auth/login.php
+++ web-projects/blog/application/views/auth/login.php
@@ -1,28 +1,29 @@
-<h1>Login</h1>
-<p>Please login with your email/username and password below.</p>
-	
-<div id="infoMessage"><?php echo $message;?></div>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>asset/css/mycss.css"/>
+<div id="login_form">
+    <h1>Login</h1>
+    <p>Please login with your email/username and password below.</p>
 
-<?php echo form_open("auth/login");?>
-  	
-  <p>
+    <div id="infoMessage"><?php echo $message;?></div>
+
+    <?php echo form_open("auth/login");?>
+
+    <p>
     <label for="identity">Email/Username:</label>
     <?php echo form_input($identity);?>
-  </p>
+    </p>
 
-  <p>
+    <p>
     <label for="password">Password:</label>
     <?php echo form_input($password);?>
-  </p>
+    </p>
 
-  <p>
+    <p>
     <label for="remember">Remember Me:</label>
     <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
-    
-    
-  <p><?php echo form_submit('submit', 'Login');?></p>
-    
-<?php echo form_close();?>
+    </p>
 
-<p><a href="forgot_password">Forgot your password?</a></p>
+
+    <p><?php echo form_submit('submit', 'Login');?></p>
+
+    <?php echo form_close();?>
+</div>
