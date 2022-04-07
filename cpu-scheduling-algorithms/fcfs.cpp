56e2d72a2c5e88206a2bad525634ce9d09e9a1ea
mac_bd@yahoo.com <forhannoor@gmail.com>
2022-04-07 17:52:21 -0400

    Code reformatting

--- cpu-scheduling-algorithms/fcfs.cpp
+++ cpu-scheduling-algorithms/fcfs.cpp
@@ -1,49 +1,60 @@
-#include<iostream>
-#include<stdlib.h>
-#include<fstream>
-#include<stdio.h>
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
 
-using namespace std;
-
-int fcfs(int array[],int size);
+int fcfs(int array[], int size);
 
 int main()
 {
-    char fileName[20];//to take file name from user
-    cout<<"Enter file name:\n";
+    // File name.
+    char fileName[20];
+    printf("File to read from:\n");
     gets(fileName);
-    int size;//number of processes to be taken from file
-    ifstream in(fileName);//input file stream to open file
+    // Number of processes.
+    int size;
+    // Input file stream to read file.
+    std::ifstream in(fileName);
     in>>size;
-    cout<<"Number of processes:\n"<<size;
+    printf("#process: %d\n", size);
     int b[size];
-    for(int i=0;i<size;i++)
+
+    for(int i = 0; i < size; i++)
     {
-        in>>b[i];//storing burst times into an array
+        // Storing burst times into an array.
+        in>>b[i];
     }
-    cout<<"\nProcess burst times are:\n";
-    for(int j=0;j<size;j++)
+
+    printf("Process burst times:\n");
+
+    for(int j = 0; j < size; j++)
     {
-        cout<<b[j]<<"\n";//printing burst times
+        printf("%d\n", b[j]);
     }
+
     fcfs(b,size);
 }
 
-int fcfs(int array[],int size)
+int fcfs(int array[], int size)
 {
     int p[size];
-    double total=0.0;
+    double total = 0.0;
     double avg;
-    p[0]=0;
-    for(int i=1;i<size;i++)
-    p[i]=p[i-1]+array[i];
-    for(int k=0;k<size;k++)
+    p[0] = 0;
+
+    for(int i = 1; i < size; i++)
     {
-        total=total+p[k];
-        cout<<"\nWaiting time for process "<<k<<":"<<p[k];
+        p[i] = p[i-1] + array[i];
     }
-    avg=total/(double)size;
-    cout<<"\nTotal waiting time in FCFS scheduling:\n"<<total;
-    cout<<"\nAverage waiting time in FCFS scheduling:\n"<<avg;
+
+    for(int k = 0; k < size; k++)
+    {
+        total = total + p[k];
+        printf("Waiting time for process#%d: %d\n", k, p[k]);
+    }
+
+    avg = total / (double) size;
+    printf("Total waiting time: %lf\n", total);
+    printf("Average waiting time: %lf\n", avg);
     return 0;
 }
